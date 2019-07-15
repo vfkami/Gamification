49fa3e113ea04bbd3df77b710dcd1edf06d12027
Yvan Brito <yvanbrito96@gmail.com>
2019-07-15 14:07:32 -0300

    Segunda Tarde

--- d4c/sbrc.py
+++ d4c/sbrc.py
@@ -318,7 +318,7 @@ var Dash = {
        {url: "http://gercom.ddns.net:8082/qrshow/3", time: 20, refresh: true},
        {url: "http://gercom.ddns.net:8082/qrshow/4", time: 20, refresh: true},
        {url: "http://gercom.ddns.net:8082/patrocinio", time: 20, refresh: false},
-       {url: "http://gercom.ddns.net:8082/segunda", time: 80, refresh: true}
+       {url: "http://gercom.ddns.net:8082/segunda", time: 160, refresh: true}
     ],
     startup: function () {
         for (var index = 0; index < Dash.dashboards.length; index++) {
@@ -606,7 +606,7 @@ def sessions():
     # quintaManha = "JAI #4(9-12),WEI(9-12),BraSNAM(9-12),BreSci(9-12),SBCUP(9-12),ENCompIP(9-12),WFIBRE(9-12),WPIETF(9-12),WASHES(9-12),COMPUTEC(9-12)"
 	# quintaTarde = "JAI #4(16-19),WCAMA(16-19),BraSNAM(16-19),BreSci(16-19),SBCUP(16-19),ENCompIP(16-19),WFIBRE(16-19),WPIETF(16-19),WASHES(16-19),COMPUTEC(16-19),SECOMU"
     # session = domingo
-    return "JAI #1(9-12),WEI(9-12),CTD(9-12),WIT(9-12),SEMISH(9-12),SEMISH-hackaton(9-12),WPerformance(9-12),WTranS(9-12),ETC(9-12),CLOUDSCAPE-Brazil(9-12),ForumPG(9-12)"
+    return "JAI #1(16-19),WEI(16-19),CTD(16-19),WIT(16-19),SEMISH(16-19),SEMISH-hackaton(16-19),WPerformance(16-19),WTranS(16-19),ETC(16-19),CLOUDSCAPE-Brazil(16-19),ForumPG(16-19),SECOMU"
 
 
 @app.route('/patrocinio')
